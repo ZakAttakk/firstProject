efab0e3afbd078fde9764344be47bfe411b9b1bd
ZakAttakk <zjbrewer@gmail.com>
2023-06-27 19:19:12 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+// var myName = "Zach"
+// console.log(myName)
+
+// var numberOne = 15
+// var numberTwo = 2
+
+// var answer = numberOne / numberTwo
+
+// console.log(answer)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var names = ["Zach", "Tom"]
+
+// console.log(names[3])
+
+for (let i = 0; i < names.length; i++){
+  console.log(names[i])
+}
+
